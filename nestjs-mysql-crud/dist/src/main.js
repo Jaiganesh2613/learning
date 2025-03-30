013bd166f8f51a8ee9840cba137ab33626d641a2
rpitchaikannu <rpitchaikannu@thapovan-inc.com>
2025-03-30 14:56:53 +0530

    Added Interceptor

--- nestjs-mysql-crud/dist/src/main.js
+++ nestjs-mysql-crud/dist/src/main.js
@@ -2,8 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const exception_interceptor_1 = require("./interceptors/exception.interceptor");
+const logging_interceptor_1 = require("./interceptors/logging.interceptor");
+const transform_interceptor_1 = require("./interceptors/transform.interceptor");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.useGlobalInterceptors(new logging_interceptor_1.LoggingInterceptor());
+    app.useGlobalInterceptors(new transform_interceptor_1.TransformInterceptor());
+    app.useGlobalFilters(new exception_interceptor_1.GlobalExceptionFilter());
     await app.listen(process.env.PORT ?? 3004);
 }
 bootstrap();
--- nestjs-mysql-crud/src/main.ts
+++ nestjs-mysql-crud/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './interceptors/exception.interceptor';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 3004);
 }
 bootstrap();
